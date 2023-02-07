c26f22b070d1cc3543f5c3b34a77f3baeffcf4ad
ciga <vajjucigan12@gmail.com>
2023-02-07 15:54:50 +0100

    uzenet

---
+++ script.js
@@ -0,0 +1,13 @@
+console.dir(document);
+//document.title="ciga"
+//document.log(document.head)
+//documenty.all[8].textContent="drugi naslov"
+//console.log(document.getElementById("main_title"))
+//document.getElementById("main_title").textContent="3 naslov"
+//let cim= document.getElementById("main_title")
+//cim.textContent="4 naslov"
+for(let i=0;i<items.lenght;i++){
+    if(i%2==0){
+        item[i].style.backgroundColor="lightgrey";
+    }
+}
