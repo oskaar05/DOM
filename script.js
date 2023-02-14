a165bd76ac8efcd729178efa02145c35c9b17450
ciga <vajjucigan12@gmail.com>
2023-02-14 12:17:12 +0100

    2nd chance

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 console.dir(document);
-//document.title="ciga"
+//document.title="elso nev"
 //document.log(document.head)
-//documenty.all[8].textContent="drugi naslov"
-//console.log(document.getElementById("main_title"))
-//document.getElementById("main_title").textContent="3 naslov"
-//let cim= document.getElementById("main_title")
-//cim.textContent="4 naslov"
-for(let i=0;i<items.lenght;i++){
-    if(i%2==0){
-        item[i].style.backgroundColor="lightgrey";
-    }
-}
+// documenty.all[8].textContent = "Masik cim"
+//console.log(document.getElementById("main_titile"));
+//document.getElementById("main_title").textContent = "harmadik cim";
+//let cim = document.getElementById("main_title");
+//cim.textContent = "negyedik cim";
+/*for (let i = 0; i < items.lenght; i++) {
+  if (i % 2 == 0) {
+    item[i].style.backgraundColor = "lightgrey";
+  }
+}*/
+/*console.log(document.getElementsByTagName("h2"))
+let h2=document.getElementsByTagName*/
+//id #
+//class .
+console.log(document.querySelector("elso a"));
+let elsolink = document.querySelector("#elso a");
+elsolink.style.color = "red";
+
+console.log(document.querySelector("elso p"));
+let elsolink = document.querySelector("#elso p");
+elsolink.style.color = "red";
